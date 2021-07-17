afaaade4b8ceb35ee88aaa3076b242283cc5babf
Angel de Jesús Limones Quirino <43385232+C1TR0N5@users.noreply.github.com>
2021-07-17 12:16:22 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-alert()
+$(".agregar").on("click", function (e) {
+  e.preventDefault();
+  $(".Lista").append(`<div> 
+        <li class = "lis"> 
+            <p class = "itemShop">${$("#newText").val()}</p>
+            <button class="checar"> Check </button>
+            <button class="del" > Delete </button>
+        </li> </div>`);
+});
+
+$(document).on("click", ".checar", function (e) {
+  e.preventDefault();
+  $(this).parent().toggleClass("chec");
+});
+
+$(document).on("click", ".del", function (e) {
+  e.preventDefault();
+  $(this).parent().remove();
+});
